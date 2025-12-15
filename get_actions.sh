1e01146b0d04ee620305fb037448c622a1c851bb
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2025-12-14 23:11:51 -0500

    Working actions curling

--- get_actions.sh
+++ get_actions.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-websrc="https://charachorder.io"
-
-devices_json=$(curl -X 'GET' \
-  "${websrc}/firmware/" \
-  -H 'accept: application/json')
+websrc="https://charachorder.io/firmware"
 
 get_name_from_json () {
 	json="$1"
@@ -12,6 +8,41 @@ get_name_from_json () {
 		sed 's/,//'
 }
 
-device_names=$(get_name_from_json "$devices_json")
+get_device_list() {
+	devices_json=$(curl -X 'GET' \
+		"${websrc}/" \
+		-H 'accept: application/json')
+	echo "$devices_json"
+}
 
-echo "$device_names" | fzf
+
+get_firmware_list() {
+	device="$1"
+	curl -X 'GET' "${websrc}/${device}/" -H 'accept: application/json'
+}
+
+get_firmware_list() {
+	device="$1"
+	curl -X 'GET' "${websrc}/${device}/" -H 'accept: application/json'
+}
+
+get_metadata() {
+	device="$1"
+	firmware="$2"
+	dataset_name="actions.json"
+	dataset_name="$3"
+	curl -X 'GET' \
+		"${websrc}/${device}/${firmware}/${dataset_name}" \
+		-H 'accept: */*'
+}
+
+#device_names=$(get_name_from_json "$(get_device_list)")
+#device=$(echo "$device_names" | fzf)
+#firmware_names=$(get_name_from_json "$(get_firmware_list $device)")
+#firmware=$(echo "$firmware_names" | fzf)
+device="m4g_s3"
+firmware="3.0.0-gamma.1"
+echo "$device $firmware"
+action_names="$(get_metadata $device $firmware 'actions.json')"
+echo "$action_names"
+#firmware_ver=$(get_name_from_json "$(get_firmware_list $device)")
